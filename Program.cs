218bc9882d7b0d85449fac5a94d922ed5dceb6d0
ftaleb <frederik.taleb@gmail.com>
2021-10-21 15:03:40 -0400

    Code troué

--- Program.cs
+++ Program.cs
@@ -25,56 +25,77 @@ namespace Labo_Tour_de_Zork_Combat_01
             int dommagePustus = 11;
             int initiativePustus = 0;
 
-
-
-            // Celui qui a l'initiative la plus haute frappe en prmier
-            // Si l'autre n'est pas mort, il réplique avec ses dommages
-            // Quand l'initiative est égales, les 2 dommages sont assignés sans exception
-            initiativeSergent = GenererInitiative(agiliteSergent);
-            initiativePustus = GenererInitiative(agilitePustus);
-            if (initiativeSergent > initiativePustus)
+            // Le combat sera fini quand un des deux personnage arrive à 0 pv
+            while (pvSergent > 0 && pvPustus > 0)
             {
-                // Il faut vérifier l'armure avant d'enlever des points de vie
-                // En enlevant toujours les dégât à l'armure on fini par avoir une nombre négatif
-                // Quand on a un nombre négatif, on enlève ce nombre aux points de vie et on remet l'armure à 0 
-                // pour ne pas fausser les calculs subséquents
-                //
-                // Il serait possible de gérer l'assignation des dommages avec une méthode, mais je vous épargne
-                // le mal de tête
-                armurePustus -= GenererDommages(dommageSergent);
-                if (armurePustus < 0)
+                // Celui qui a l'initiative la plus haute frappe en prmier
+                // Si l'autre n'est pas mort, il réplique avec ses dommages
+                // Quand l'initiative est égales, les 2 dommages sont assignés sans exception
+                initiativeSergent = GenererInitiative(agiliteSergent);
+                initiativePustus = GenererInitiative(agilitePustus);
+                if (initiativeSergent > initiativePustus)
                 {
-                    // la variable d'armure du personnage contient un nombre négatif
-                    // il représente le nombre de dommages ayant dépassé l'armure
-                    // donc on addition le nombre négatif aux points de vie pour avoir un 
-                    // résultat cohérent
-                    // ex: armurePustus -3 & pvPustus 30 
-                    //     en faisant pvPustus += armurePustus on a 30 + -3
-                    pvPustus += armurePustus;
-                    // Il ne faut pas oublier de remttre l'armure à 0, sinon on a encore les dommages du tour précédent
-                    armurePustus = 0;
+                    // Il faut vérifier l'armure avant d'enlever des points de vie
+                    // En enlevant toujours les dégât à l'armure on fini par avoir une nombre négatif
+                    //
+                    // Quand on a un nombre négatif, on enlève ce nombre aux points de vie et on remet l'armure à 0 
+                    // pour ne pas fausser les calculs subséquents
+                    //
+                    // Il serait possible de gérer l'assignation des dommages avec une méthode, mais je vous épargne
+                    // le mal de tête
+                    armurePustus -= GenererDommages(dommageSergent);
+                    if (armurePustus < 0)
+                    {
+                        // la variable d'armure du personnage contient un nombre négatif
+                        // il représente le nombre de dommages ayant dépassé l'armure
+                        // donc on addition le nombre négatif aux points de vie pour avoir un 
+                        // résultat cohérent
+                        // ex: armurePustus -3 & pvPustus 30 
+                        //     en faisant pvPustus += armurePustus on a 30 + -3
+                        pvPustus += armurePustus;
+                        // Il ne faut pas oublier de remttre l'armure à 0, sinon on a encore les dommages du tour précédent
+                        armurePustus = 0;
+                    }
+                    // On assigne les dégâts au premier personnage seulement si le 2ème est encore vivant
+                    if (pvPustus > 0)
+                    {
+                        armureSergent -= GenererDommages(dommagePustus);
+                        if (armureSergent < 0)
+                        {
+                            pvSergent += armureSergent;
+                            armureSergent = 0;
+                        }
+                    }
                 }
-                if (pvPustus > 0)
+                else if (initiativePustus > initiativeSergent)
                 {
+                    // On répète le processus mais dans un ordre différent
                     armureSergent -= GenererDommages(dommagePustus);
                     if (armureSergent < 0)
                     {
                         pvSergent += armureSergent;
                         armureSergent = 0;
                     }
+                    if (pvSergent > 0)
+                    {
+                        armurePustus -= GenererDommages(dommageSergent);
+                        if (armurePustus < 0)
+                        {
+                            pvPustus += armurePustus;
+                            armurePustus = 0;
+                        }
+                    }
                 }
-            }
-            else if (initiativePustus > initiativeSergent)
-            {
-                // On répète le processus mais dans un ordre différent
-                armureSergent -= GenererDommages(dommagePustus);
-                if (armureSergent < 0)
+                else
                 {
-                    pvSergent += armureSergent;
-                    armureSergent = 0;
-                }
-                if (pvSergent > 0)
-                {
+                    // Si les 2 personnages se frappent simultanément c'est encore le même processus mais 
+                    // les dommages sont assignés même si les points de vie du premier tombent sous 0
+                    armureSergent -= GenererDommages(dommagePustus);
+                    if (armureSergent < 0)
+                    {
+                        pvSergent += armureSergent;
+                        armureSergent = 0;
+                    }
                     armurePustus -= GenererDommages(dommageSergent);
                     if (armurePustus < 0)
                     {
@@ -82,26 +103,17 @@ namespace Labo_Tour_de_Zork_Combat_01
                         armurePustus = 0;
                     }
                 }
-            }
-            else
-            {
-                // Si les 2 personnages se frappent simultanément c'est encore le même processus mais 
-                // les dommages sont assignés même si les points de vie du premier tombent sous 0
-                armureSergent -= GenererDommages(dommagePustus);
-                if (armureSergent < 0)
-                {
-                    pvSergent += armureSergent;
-                    armureSergent = 0;
-                }
-                armurePustus -= GenererDommages(dommageSergent);
-                if (armurePustus < 0)
-                {
-                    pvPustus += armurePustus;
-                    armurePustus = 0;
-                }
+
+                // Afficher les stats défensive de chaque personnage à la fin du tour
+                Console.WriteLine($"{nomPustus} armure: {armurePustus}  pv: {pvPustus}");
+                Console.WriteLine($"{nomSergent} armure: {armureSergent} pv: {pvSergent}");
+                Console.WriteLine("Appuyer sur une touche pour continuer");
+                Console.ReadKey();
+                Console.Clear();
             }
 
-            // Fin du tour, on affiche le résultat
+            // Fin du combat, on affiche le résultat
+            Console.Clear();
             if (pvSergent <= 0 && pvPustus > 0)
             {
                 Console.WriteLine($"{nomSergent} est mort, {nomPustus} a gagné");
@@ -110,14 +122,11 @@ namespace Labo_Tour_de_Zork_Combat_01
             {
                 Console.WriteLine($"{nomPustus} est mort, {nomSergent} a gagné");
             }
-            else if (pvPustus <= 0 && pvSergent <= 0)
+            else
             {
                 Console.WriteLine($"Les deux combatants sont morts!");
             }
-            else
-            {
-                Console.WriteLine($"Le combat continu, {nomSergent} et {nomPustus} sont encore debout!");
-            }
+            Console.ReadKey();
         }
 
         /**
